84c90133584679b2cfa7624966cf203eb174433c
dcMalay <dc.malay2022@gmail.com>
2022-10-25 18:39:16 +0530

    make the function to upload all the imagefiles

--- lib/views/post_products/post_products.dart
+++ lib/views/post_products/post_products.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:julia/const/const.dart';
 import 'package:julia/data/model/dynamic_form_model.dart';
 import 'package:julia/data/repository/dynamic_form_repo.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:http/http.dart' as http;
 
 // ignore: must_be_immutable
 class PostProductsView extends StatefulWidget {
@@ -76,107 +79,34 @@ class _PostProductsViewState extends State<PostProductsView> {
 
     dio
         .post("http://mouldstaging.com/upload.php", data: data)
-        .then((response) => print(response))
+        .then((response) => postProductData(response.data))
         .catchError((error) => print(error));
   }
 
-//   static Future<String> uploadMultipleImage({required List<File> files}) async {
-// // string to uri
-//     var uri = Uri.parse("your api url");
-//     print("image upload URL - $uri");
-// // create multipart request
-//     var request = new http.MultipartRequest("POST", uri);
-
-//     for (var file in files) {
-//       String fileName = file.path.split("/").last;
-//       var stream = http.ByteStream(DelegatingStream.typed(file.openRead()));
-
-//       // get file length
-
-//       var length = await file.length(); //imageFile is your image file
-//       print("File lenght - $length");
-//       print("fileName - $fileName");
-//       // multipart that takes file
-//       var multipartFileSign =
-//           http.MultipartFile('images[]', stream, length, filename: fileName);
-
-//       request.files.add(multipartFileSign);
-//     }
-
-//     Map<String, String> headers = {
-//       "Accept": "application/json",
-//       "Authorization":
-//           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMiwiZXhwIjoxNjE3NTQyNDE0LCJpc3MiOiJsb2NhbGhvc3QiLCJpYXQiOjE2MTcxODI0MTR9.dGRbINOdx_tf417fpsjdQ5CR7uGULs98FjLGm2w4kRY"
-//     }; // ignore this headers if there is no authentication
-//     print("headers - $headers}");
-// //add headers
-//     request.headers.addAll(headers);
-
-// //adding params
-//     request.fields['heading'] = "heading";
-//     request.fields['description'] = "description";
-//     request.fields['mobile'] = "mobile";
-//     request.fields['email'] = "email";
-//     request.fields['category'] = "1";
-//     request.fields['location_type'] = "1";
-//     request.fields['location'] = "location";
-//     request.fields['lat'] = "12";
-//     request.fields['lng'] = "123";
-//     request.fields['price'] = "1231";
-//     request.fields['sub_category'] = "3";
-
-// // send
-//     var response = await request.send();
-
-//     print(response.statusCode);
-
-//     var res = await http.Response.fromStream(response);
-//     if (response.statusCode == 200 || response.statusCode == 201) {
-//       print("Item form is statuscode 200");
-//       print(res.body);
-//       var responseDecode = json.decode(res.body);
-
-//       if (responseDecode['status'] == true) {
-//         return res.body;
-//       } else {
-//         return res.body;
-//       }
-//     }
-//   }
-
-  // postProductData() async {
-  //   for (var i = 0; i < images.length; i++) {
-  //     ByteData byteData = await images[i].getByteData();
-  //     List<int> imageData = byteData.buffer.asInt8List();
-
-  //     http.MultipartFile multipartFile = MultipartFile.fromBytes(
-  //       images[i].name!,
-  //       imageData,
-  //     );
-  //     var authToken = await _secureStorage.read(key: 'token');
-  //     var authUser = await _secureStorage.read(key: 'userId');
-  //     print(authToken);
-  //     print(_locationValue);
-  //     var response = http.post(Uri.parse('$baseUrl/user/create/new/ad'),
-  //         headers: {
-  //           HttpHeaders.authorizationHeader: authToken!,
-  //           HttpHeaders.contentTypeHeader: "application/json"
-  //         },
-  //         body: jsonEncode(<String, dynamic>{
-  //           "post_category": widget.categoryId,
-  //           "post_subcategory": widget.subCategoryId,
-  //           "post_user_id": authUser,
-  //           "fields": '',
-  //           "post_location": _locationValue,
-  //           "post_title": titleController.text,
-  //           "post_image": multipartFile.filename,
-  //           "post_price": priceController.text,
-  //           "post_description": descController.text,
-  //           "auth_name": nameController.text,
-  //         }));
-  //     return response;
-  //   }
-  // }
+  postProductData(String imageName) async {
+    var authToken = await _secureStorage.read(key: 'token');
+    var authUser = await _secureStorage.read(key: 'userId');
+    print(authToken);
+    print(_locationValue);
+    var response = http.post(Uri.parse('$baseUrl/user/create/new/ad'),
+        headers: {
+          HttpHeaders.authorizationHeader: authToken!,
+          HttpHeaders.contentTypeHeader: "application/json"
+        },
+        body: jsonEncode(<String, dynamic>{
+          "post_category": widget.categoryId,
+          "post_subcategory": widget.subCategoryId,
+          "post_user_id": authUser,
+          "fields": '',
+          "post_location": _locationValue,
+          "post_title": titleController.text,
+          "post_image": imageName,
+          "post_price": priceController.text,
+          "post_description": descController.text,
+          "auth_name": nameController.text,
+        }));
+    return response;
+  }
 
   Widget buildGridView() {
     return GridView.count(
